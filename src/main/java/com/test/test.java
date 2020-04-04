4d0503e7b76a9ad69e649be2d4daa22b4b231d57
huangpeng <huangpeng@sensorsdata.cn>
2020-04-04 22:12:35 +0800

    commit

---
+++ src/main/java/com/test/test.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.apache.commons.collections.map.HashedMap;
+
+import java.util.Map;
+
+public class test {
+
+    public static void main(String[] args) {
+        Map<String, Object> map = new HashedMap();
+        map.put("id", 1l);
+
+
+    }
+}
